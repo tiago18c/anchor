aaa6bdb50acd79bfb69cc87204046f6cd85a08a7
acheron <98934430+acheroncrypto@users.noreply.github.com>
2026-03-13 13:15:59 +0100

    tests: Fix `errors` (#4311)

--- tests/errors/test.sh
+++ tests/errors/test.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 # We don't want to build `multiple-errors` because it is expected to error
-anchor build -p errors --skip-lint
+anchor build -p errors --skip-lint --ignore-keys
 anchor test --skip-build
