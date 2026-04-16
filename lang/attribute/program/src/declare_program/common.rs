df44381fb13353a97d5d498aa0f03147f5b5fbfd
Jamie Hill-Daniel <134328753+jamie-osec@users.noreply.github.com>
2026-04-16 12:10:41 +0100

    lang: Fix name collision in composite account de-duplicator (#4401)

--- lang/attribute/program/src/declare_program/common.rs
+++ lang/attribute/program/src/declare_program/common.rs
@@ -450,42 +450,44 @@ pub fn get_all_instruction_accounts(idl: &Idl) -> Vec<IdlInstructionAccounts> {
         .iter()
         .flat_map(|ix| ix.accounts.to_owned())
         .collect::<Vec<_>>();
-    get_non_instruction_composite_accounts(&ix_accs, idl)
-        .into_iter()
-        .fold(Vec::<IdlInstructionAccounts>::default(), |mut all, accs| {
-            // Make sure they are unique
-            if all.iter().all(|a| a.accounts != accs.accounts) {
-                // The name is not guaranteed to be the same as the one used in the actual source
-                // code of the program because the IDL only stores the field names
-                let name = if all.iter().all(|a| a.name != accs.name) {
-                    accs.name.to_owned()
-                } else {
-                    // Append numbers to the field name until we find a unique name
-                    #[allow(
-                        clippy::expect_used,
-                        reason = "unbounded integer search always finds a free slot"
-                    )]
-                    let unique = (2..)
-                        .find_map(|i| {
-                            let name = format!("{}{i}", accs.name);
-                            all.iter().all(|a| a.name != name).then_some(name)
-                        })
-                        .expect("Should always find a valid name");
-                    unique
-                };
 
-                all.push(IdlInstructionAccounts {
-                    name,
-                    accounts: accs.accounts.to_owned(),
-                })
-            }
-
-            all
-        })
-        .into_iter()
-        .chain(idl.instructions.iter().map(|ix| IdlInstructionAccounts {
+    let instruction_accounts: Vec<IdlInstructionAccounts> = idl
+        .instructions
+        .iter()
+        .map(|ix| IdlInstructionAccounts {
             name: ix.name.to_owned(),
             accounts: ix.accounts.to_owned(),
-        }))
-        .collect()
+        })
+        .collect();
+
+    get_non_instruction_composite_accounts(&ix_accs, idl)
+        .into_iter()
+        .fold(instruction_accounts.clone(), |mut all, accs| {
+            // Skip if the account layout is already represented.
+            if all.iter().any(|a| a.accounts == accs.accounts) {
+                return all;
+            }
+
+            let name = if all.iter().all(|a| a.name != accs.name) {
+                accs.name.to_owned()
+            } else {
+                // Append numbers to the field name until we find a unique name
+                #[allow(
+                    clippy::expect_used,
+                    reason = "unbounded integer search always finds a free slot"
+                )]
+                (2..)
+                    .find_map(|i| {
+                        let name = format!("{}{i}", accs.name);
+                        all.iter().all(|a| a.name != name).then_some(name)
+                    })
+                    .expect("Should always find a valid name")
+            };
+
+            all.push(IdlInstructionAccounts {
+                name,
+                accounts: accs.accounts.to_owned(),
+            });
+            all
+        })
 }
